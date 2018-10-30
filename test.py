ae5c7cd189c7e9599a4be433c0f54552fe81c407
cmaliwal <chiragmaliwal1995@gmail.com>
2018-10-30 14:58:33 +0530

    added basics test scripts

---
+++ test.py
@@ -0,0 +1,16 @@
+from selenium import webdriver 
+import time
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome(" ")
+#driver = webdriver.Firefox() 
+#driver = webdriver.Ie()
+
+driver.set_page_load_timeout(10)
+
+driver.get("http://google.com")
+driver.find_element_by_name("q").send_keys("simple is better than complex")
+
+driver.find_element_by_name("btnK").send_keys(Keys.ENTER)
+time.sleep(4)
+driver.quit()
